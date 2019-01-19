8d6dd6c726c90d57f7ca19ac6dd62febf861797a
Jimmy Blundell <jblundell123@gmail.com>
2019-01-19 15:38:38 -0600

    Added controls, introduced custom button class to add border using awakefromNib

---
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh-app
+//
+//  Created by Jimmy Blundell on 1/19/19.
+//  Copyright © 2019 Jimmy Blundell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swoosh-app/swoosh-app/Views/BorderButton.swift
@@ -0,0 +1,19 @@
+//
+//  BorderButton.swift
+//  swoosh-app
+//
+//  Created by Jimmy Blundell on 1/19/19.
+//  Copyright © 2019 Jimmy Blundell. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+}
